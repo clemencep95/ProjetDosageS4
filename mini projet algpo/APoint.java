47c91e72875528a493bf305016bc21ed1330c98a
clemencep95 <perrot.clemence95@gmail.com>
2022-04-11 09:45:45 +0200

    apoint

--- mini projet algpo/APoint.java
+++ mini projet algpo/APoint.java
@@ -1,19 +1,23 @@
 public class APoint {
     
-    public double x, y;
     
-    public APoint(double ax, double ay){
-        x = ax;
-        y = ay;
-    }
-            
-    public double distance( APoint otherPoint ) {
-        double dx = x - otherPoint.x;
-        double dy = y - otherPoint.y;
-        return Math.sqrt(dx * dx + dy * dy);
-    }
+        public double x, y;
+        
+        public APoint (double ax, double ay){
+            x = ax;
+            y = ay;
+        }
+                
+        public double distance( APoint otherPoint ) {
+            double dx = x - otherPoint.x;
+            double dy = y - otherPoint.y;
+            return Math.sqrt(dx * dx + dy * dy);
+        }
+        
+        public String toString() {
+            return  "[x=" + x + ",y=" + y + "]";
+        }  
     
-    public String toString() {
-        return  "[x=" + x + ",y=" + y + "]";
-    }  
+    
+    
 }
